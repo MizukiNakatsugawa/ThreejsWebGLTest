de3da4be82e79325187c87df6cdbadac3526c9a6
MizukiNakatsugawa <nakatsugawa@romantech.co.jp>
2021-03-05 19:42:58 +0900

    commit

--- index.js
+++ index.js
@@ -1,59 +1,69 @@
 // ページの読み込みを待つ
-window.addEventListener('load', init);
+window.addEventListener('load', main);
 
-function init() {
-  // サイズを指定
-  const width = 960;
-  const height = 540;
+function main() {
+  const canvas = document.querySelector("#myCanvas")
+  const renderer = new THREE.WebGLRenderer({canvas})
 
-  // レンダラーを作成
-  const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#myCanvas')
-  });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(width, height);
+  const fov = 75; // field of view 垂直方向に７５度
+  const aspect = 2; // 表示アスペクト　デフォルトは2
+  const near = 0.1; // near と far は、レンダリングされるカメラの前のスペースを表します。 その範囲の前またはその範囲の後はクリップされます（描画されません）
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-  // シーンを作成
-  const scene = new THREE.Scene();
+  camera.position.z = 2; // カメラはデフォルトで、Yを上にして-Z軸を見下ろします
 
-  // カメラを作成
-  const camera = new THREE.PerspectiveCamera(
-    45,
-    width / height,
-    0.1,
-    1000
-  );
-  // カメラの初期座標を設定
-  camera.position.set(0, 0, 5);
+  const scene = new THREE.Scene(); // three.jsの Scene は、シーングラフのフォームのルートです
 
-  // カメラコントローラーを作成
-  const controls = new THREE.OrbitControls(camera);
+  // ボックスのデータが含まれている BoxGeometry を作成します
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-  // 平行光源を作成
-  const directionalLight = new THREE.DirectionalLight(0xffffff);
-  directionalLight.position.set(1, 1, 1);
-  scene.add(directionalLight);
-  // 環境光を追加
-  const ambientLight = new THREE.AmbientLight(0xffffff);
-  scene.add(ambientLight);
+  const material = new THREE.MeshPhongMaterial({color: 0x44aa88}); //基本的なマテリアルを作成し、色を設定します
 
-  // 3DS形式のモデルデータを読み込む
-  const loader = new THREE.TDSLoader();
-  // テクスチャーのパスを指定
-  loader.setPath('models/textures/');
-  // 3dsファイルのパスを指定
-  loader.load('models/portalgun.3ds', object => {
-      object.scale.set(1,1,1);
-      object.position.set(0,0,0);
-    // 読み込み後に3D空間に追加
-    scene.add(object);
-  });
+  const cube = new THREE.Mesh(geometry, material); // Mesh は Geometry (オブジェクトの形状)と Material （オブジェクトの描画方法、光沢または平坦、適用する色、適用するテクステャなど）を組み合わせます
 
-  tick();
-  // 毎フレーム時に実行されるループイベントです
-  function tick() {
-    // レンダリング
-    renderer.render(scene, camera);
-    requestAnimationFrame(tick);
+  function makeInstance(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({color});
+
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    cube.position.x = x;
+
+    return cube;
   }
+
+  const cubes = [
+    makeInstance(geometry, 0x44aa88,  0),
+    makeInstance(geometry, 0x8844aa, -2),
+    makeInstance(geometry, 0xaa8844,  2),
+  ]
+  
+  // renderer.render(scene, camera);
+
+  // ディレクショナルライトを作成
+  const color = 0xFFFFFF;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+
+  function render(time) {
+    time *= 0.001;
+
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * .1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 }
